5e751e2f5728b7938adb8be8e17d68d2dad3cce4
omkar gaikwad <219820629+OmkarIammaturity@users.noreply.github.com>
2025-08-27 15:08:08 +0530

    good

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer'
 import { AuthProvide } from './context/AuthContext'
 
