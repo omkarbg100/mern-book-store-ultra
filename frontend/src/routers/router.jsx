bd23377818579f23ff55fd3c9df7a91667bb59bc
omkar gaikwad <219820629+OmkarIammaturity@users.noreply.github.com>
2025-08-27 15:10:15 +0530

    d

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App.jsx";
-import Home from "../pages/home/home.jsx";
+import Home from "../pages/home/Home.jsx";
 import Login from "../components/Login.jsx";
 import Register from "../components/Register.jsx";
 import CartPage from "../pages/Book/CartPage.jsx";
